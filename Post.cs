f06af2de8b149c572c43ff255abbdeed3ee441a6
VardanVardanyan24 <Vardanvardanyan242003@gmail.com>
2025-03-24 20:27:12 +0400

    Create Post.cs

---
+++ Post.cs
@@ -0,0 +1,10 @@
+namespace Lesson1
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
