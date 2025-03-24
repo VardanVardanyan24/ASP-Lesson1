135311b0ae5bc34d9d386ab7edb13064f2e2108e
VardanVardanyan24 <Vardanvardanyan242003@gmail.com>
2025-03-24 20:26:16 +0400

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,8 +1,12 @@
-﻿namespace Lesson1
+namespace Lesson1
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Email { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Avatar { get; set; }
     }
 }
+
